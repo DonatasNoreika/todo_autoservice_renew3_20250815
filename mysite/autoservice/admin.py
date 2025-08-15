08cad6f6e511bf47ce7c1382309f5f3aa1086c46
DonatasNoreika <donoras@gmail.com>
2025-08-15 22:56:56 +0300

    3 dalis done

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 from .models import Car, Service, Order, OrderLine
 
+
 # Register your models here.
-admin.site.register(Car)
-admin.site.register(Service)
-admin.site.register(Order)
-admin.site.register(OrderLine)
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+    fields = ['service', 'quantity', 'line_sum']
+    readonly_fields = ['line_sum']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["car", "date", "total"]
+    inlines = [OrderLineInLine]
+    fieldsets = [
+        ("General", {"fields": ['car', 'date', 'total']})
+    ]
+    readonly_fields = ['date', 'total']
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['make', 'model', 'license_plate', 'vin_code', 'client_name']
+    list_filter = ['client_name', 'make', 'model']
+    search_fields = ['license_plate', 'vin_code']
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+class OrderLineAdmin(admin.ModelAdmin):
+    list_display = ['order', 'service', 'quantity', 'line_sum']
+    fieldsets = [
+        ("General", {"fields": ['order', 'service', 'quantity', 'line_sum']})
+    ]
+    readonly_fields = ['line_sum']
+
+admin.site.register(Car, CarAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLine, OrderLineAdmin)
--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -12,6 +12,10 @@ class Car(models.Model):
     def __str__(self):
         return f"{self.make} {self.model} ({self.license_plate})"
 
+    class Meta:
+        verbose_name = 'Car'
+        verbose_name_plural = 'Cars'
+
 
 class Service(models.Model):
     name = models.CharField(verbose_name="Name", max_length=100)
@@ -20,19 +24,40 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
 
 class Order(models.Model):
     date = models.DateTimeField(verbose_name="Date", auto_now_add=True)
     car = models.ForeignKey(to="Car", on_delete=models.SET_NULL, null=True, blank=True)
 
+    def total(self):
+        total = 0
+        for line in self.lines.all():
+            total += line.line_sum()
+        return total
+
     def __str__(self):
         return f"Order {self.date}: {self.car}"
 
+    class Meta:
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
 
 class OrderLine(models.Model):
-    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE, related_name="lines")
     service = models.ForeignKey(to="Service", on_delete=models.SET_NULL, null=True, blank=True)
-    quantity = models.IntegerField(verbose_name="Quantity")
+    quantity = models.IntegerField(verbose_name="Quantity", default=1)
+
+    def line_sum(self):
+        return self.service.price * self.quantity
 
     def __str__(self):
         return f"{self.service.name} - {self.quantity} ({self.order.car})"
+
+    class Meta:
+        verbose_name = 'Order Line'
+        verbose_name_plural = 'Order Lines'
