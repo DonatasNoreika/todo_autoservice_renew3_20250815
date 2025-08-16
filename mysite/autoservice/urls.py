f2a546831372fc2974f9477edca3e888480426d2
DonatasNoreika <donoras@gmail.com>
2025-08-16 10:14:16 +0300

    5 dalis done

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, cars, car
+from .views import OrderListView, OrderDetailView
 
 urlpatterns = [
-    path("", index, name="index"),
+    path('', index, name="index"),
+    path('cars', cars, name="cars"),
+    path('cars/<int:car_id>', car, name="car"),
+    path('orders/', OrderListView.as_view(), name="orders"),
+    path('orders/<int:pk>', OrderDetailView.as_view(), name="order"),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Service, Order, Car
+from django.views import generic
+
 
 def index(request):
     context = {
@@ -9,3 +11,23 @@ def index(request):
 
     }
     return render(request, template_name="index.html", context=context)
+
+
+def cars(request):
+    return render(request, template_name="cars.html", context={"cars": Car.objects.all()})
+
+
+def car(request, car_id):
+    return render(request, template_name="car.html", context={"car": Car.objects.get(pk=car_id)})
+
+
+class OrderListView(generic.ListView):
+    model = Order
+    template_name = "orders.html"
+    context_object_name = "orders"
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = "order.html"
+    context_object_name = "order"
